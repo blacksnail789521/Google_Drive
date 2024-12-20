36efd06396029f5df8ddba63fc46ed0c0bfb1d13
blacksnail789521 <blacksnail789521@gmail.com>
2024-12-20 16:21:36 +0800

    update

--- _test4.py
+++ _test4.py
@@ -17,14 +17,14 @@ tools = list(tabular_times_dict.keys())
 tabular_times = list(tabular_times_dict.values())
 time_series_times = list(time_series_times_dict.values())
 
-# Create a plot
+# Create a plot with numbers displayed on the bars
 plt.figure(figsize=(8, 5))
 x = np.arange(len(tools))  # Position of bars on the x-axis
 
 # Bar chart for the running times
 bar_width = 0.35
-plt.bar(x - bar_width/2, tabular_times, width=bar_width, label='Tabular Model', color='blue')
-plt.bar(x + bar_width/2, time_series_times, width=bar_width, label='Time Series Model', color='orange')
+bars1 = plt.bar(x - bar_width/2, tabular_times, width=bar_width, label='Tabular Model', color='blue')
+bars2 = plt.bar(x + bar_width/2, time_series_times, width=bar_width, label='Time Series Model', color='orange')
 
 # Adding labels, title, and legend
 plt.title('Running Time Comparison for Two Tools')
@@ -32,6 +32,12 @@ plt.xlabel('Tools')
 plt.ylabel('Running Time (seconds)')
 plt.xticks(x, tools)
 plt.legend()
+
+# Adding numbers on top of the bars
+for bar in bars1:
+    plt.text(bar.get_x() + bar.get_width()/2, bar.get_height() + 0.2, f'{bar.get_height()}', ha='center', fontsize=10)
+for bar in bars2:
+    plt.text(bar.get_x() + bar.get_width()/2, bar.get_height() + 0.2, f'{bar.get_height()}', ha='center', fontsize=10)
 
 # Show the plot
 plt.tight_layout()
